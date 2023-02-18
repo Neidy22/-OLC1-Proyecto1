1352949a163565b681bd39fff58ad70b2795aaa8
unknown <3236875710511@ingenieria.usac.edu.gt>
2023-02-17 19:42:16 -0600

    UI

---
+++ EXREGAN/src/exregan/EXREGAN.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exregan;
+
+import ui.mainMenu;
+
+/**
+ *
+ * @author neidy
+ */
+public class EXREGAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static mainMenu menu = new mainMenu();
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu.setVisible(true);
+    }
+    
+}
